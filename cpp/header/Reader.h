7763329a77d96f3a7c20bedf65a0ca888a3ed9a6
Bryan Gabe <46936654+BryanGabe00@users.noreply.github.com>
2022-02-09 12:46:51 -0600

    Added the outline for BinaryReader

--- cpp/header/Reader.h
+++ cpp/header/Reader.h
@@ -4,24 +4,24 @@
 
 
 class Reader {
-    public:
-        bool NextElementIsMaterial();
-        bool NextElementIsLight();
-        bool NextElementIsMesh();
-        MeshData NextMesh();
-        LightData NextLight();
-        MaterialData NextMaterial();
+public:
+    bool NextElementIsMaterial();
+    bool NextElementIsLight();
+    bool NextElementIsMesh();
+    MeshData NextMesh();
+    LightData NextLight();
+    MaterialData NextMaterial();
 
-        Reader();
+    Reader();
 
-        
-    private:
+
+private:
     /**
      * @todo These values may not work and will need to be changed as we test
-     * 
+     *
      */
-        const char MATERIAL = (char) 1;
-        const char LIGHT = (char) 2;
-        const char MESH = (char) 4;
-}
+    const char MATERIAL = (char)1;
+    const char LIGHT = (char)2;
+    const char MESH = (char)4;
+};
 
--- cpp/header/ReaderData.h
+++ cpp/header/ReaderData.h
@@ -8,88 +8,88 @@
 using namespace std;
 
 class Vector2 {
-    public:
-        float x, y;
+public:
+    float x, y;
 
-        Vector2(float X, float Y);
-}
+    Vector2(float X, float Y);
+};
 
 class Vector3 {
-    public:
-        float x, y, z;
+public:
+    float x, y, z;
 
-        Vector3(float X, float Y, float Z);
-}
+    Vector3(float X, float Y, float Z);
+};
 
- class SubMeshData {
-     public:
-        list<int> tris;
-        string materialName;
+class SubMeshData {
+public:
+    list<int> tris;
+    string materialName;
 
-        SubMeshData();
- }
+    SubMeshData();
+};
 
- class MeshData {
-    public:
-        string name;
-        list<Vector3> verts;
-        list<Vector2> uuverts;
-        list<Vector2> uuverts2;
-        list<SubMeshData> submeshes;
+class MeshData {
+public:
+    string name;
+    list<Vector3> verts;
+    list<Vector2> uuverts;
+    list<Vector2> uuverts2;
+    list<SubMeshData> submeshes;
 
-        MeshData();
- }
+    MeshData();
+};
 
-  class ColorData {
-     public:
-        double r;
-        double g;
-        double b;
-        double a;
+class ColorData {
+public:
+    double r;
+    double g;
+    double b;
+    double a;
 
-        ColorData();
- }
+    ColorData();
+};
 
- class LightData {
-     public:
-        double x;
-        double y;
-        double z;
-        double intensity;
-        ColorData colorData;
+class LightData {
+public:
+    double x;
+    double y;
+    double z;
+    double intensity;
+    ColorData colorData;
 
-        LightData();
- }
+    LightData();
+};
 
- class MapData {
-     public:
-        double xTiling;
-        double yTiling;
-        double rotation;
-        list<byte> data;
-        string extension;
+class MapData {
+public:
+    double xTiling;
+    double yTiling;
+    double rotation;
+    list<byte> data;
+    string extension;
 
-        MapData();
- }
+    MapData();
+};
 
- class MaterialData {
-    public:
-        string name;
-        bool isTransparent;
-        double reflectivity;
-        ColorData mainColor;
-        MapData textureMap;
-        MapData bumpMap;
-        MapData normalMap;
+class MaterialData {
+public:
+    string name;
+    bool isTransparent;
+    double reflectivity;
+    ColorData mainColor;
+    MapData textureMap;
+    MapData bumpMap;
+    MapData normalMap;
 
-        MaterialData();
- }
+    MaterialData();
+};
 
- class CameraData {
-     public:
-        string name;
-        double xLocation, yLocation, zLocation;
-        double xRotation, yRotation, zRotation;
+class CameraData {
+public:
+    string name;
+    double xLocation, yLocation, zLocation;
+    double xRotation, yRotation, zRotation;
 
-        CameraData();
- }
+    CameraData();
+};
