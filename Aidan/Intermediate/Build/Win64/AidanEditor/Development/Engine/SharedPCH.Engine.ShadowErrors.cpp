11740adb7172f07d68cab95e2a3b6d637d1a7315
BuildTools <gabebryan0@gmail.com>
2022-04-04 01:55:08 -0500

    Added main menu

--- Aidan/Intermediate/Build/Win64/AidanEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30141
-#include "C:/Users/malik/OneDrive/Desktop/Software Engineering Capstone/Capstone 2/Aidan/Aidan/Intermediate/Build/Win64/AidanEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Aidan/Intermediate/Build/Win64/AidanEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30141
-#include "C:/Users/malik/OneDrive/Desktop/Software Engineering Capstone/Capstone/Aidan/Aidan/Intermediate/Build/Win64/AidanEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
